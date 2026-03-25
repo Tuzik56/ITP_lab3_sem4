241560a0a49f65b0e4339468ce3d137f53837dc0
Tuzik56 <124811887+Tuzik56@users.noreply.github.com>
2026-03-25 13:14:31 +0300

    Initial commit

---
+++ src/main/java/com/example/ITP_lab3_sem4/ItpLab3Sem4Application.java
@@ -0,0 +1,13 @@
+package com.example.ITP_lab3_sem4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItpLab3Sem4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItpLab3Sem4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ITP_lab3_sem4/ItpLab3Sem4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ITP_lab3_sem4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItpLab3Sem4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
